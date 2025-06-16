8d121bf3fd996a591e2225b87590370dadd47f2d
probir sarkar <sarkarprobir1@hotmail.com>
2025-06-16 11:39:15 +0530

    aniboard-login-date-16062025

--- AniBoard_Admin/Controllers/HomeController.cs
+++ AniBoard_Admin/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace AniBoard_Admin.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
