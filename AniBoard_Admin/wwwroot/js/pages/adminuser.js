98e7a6108246fb55f1b7a5aca756ebd0bd5f52cf
SunilMurarka <sm@hellocph.com>
2025-07-03 16:40:41 +0530

    update dt. 03.07.2025

--- AniBoard_Admin/wwwroot/js/pages/adminuser.js
+++ AniBoard_Admin/wwwroot/js/pages/adminuser.js
@@ -6,3 +6,51 @@ function searchuser() {
         alert(searchText);
     }
 }
+
+function showaddedit_modal(userId) {
+    $("#adminuser_body").empty();
+    //var divspinner = '<div class="spinner-border text-success" role="status"><span class="visually-hidden"></span></div>';
+    //$("#edit_account_modal").val(divspinner);
+    //$('#edit_log_partial_caption').text(id);
+    $.ajax({
+        url: "/AdminUser/ShowAddEditModal",
+        type: "GET",
+        data: { "userId": userId },
+        datatype: "json",
+        success: function (data) {
+            $("#adminuser_body").html(data);
+            //$("#AdminUser-modal").modal('show');
+        },
+    });
+}
+
+function deleteadminuser(userId) {
+    var data = {
+        adminId: userId,
+        isDelete: 1
+    };
+    var formData = new FormData();
+    formData.append('data', JSON.stringify(data));
+    $.ajax({
+        type: "POST",
+        url: '/AdminUser/SaveAdminUser',
+        data: formData,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data.ok) {
+                alert('User deleted sucessfully');
+                setTimeout(function () {
+                    window.location.href = '/AdminUser/AdminUserList';
+                }, 1000);
+            } else {
+                alert(data.errmsg);
+            }
+        }
+    });
+
+    return false;
+
+
+}
--- Workflow/Service/Interface/IAPIService.cs
+++ Workflow/Service/Interface/IAPIService.cs
@@ -9,5 +9,6 @@ namespace Workflow.Service.Interface
     public interface IAPIService
     {
         Task<T?> GetAsync<T>(string endpoint, Dictionary<string, string?>? queryParams = null);
+        Task<TResponse?> PostAsync<TRequest, TResponse>(string endpoint, TRequest data);
     }
 }
