68c3dbab8bfe1a2fbd18df578f89952d868cfd07
SunilMurarka <sm@hellocph.com>
2025-07-09 16:30:25 +0530

    update user form dt. 09.07.2025

---
+++ AniBoard_Admin/wwwroot/js/pages/AddEditUser.js
@@ -0,0 +1,53 @@
+﻿document.querySelector('.toggle-password').addEventListener('click', function () {
+    const input = document.querySelector('#userpassword');
+    const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+    input.setAttribute('type', type);
+    this.classList.toggle('fa-eye');
+    this.classList.toggle('fa-eye-slash');
+});
+
+function saveuserdata() {
+    var uid = $('#userId').val();
+    var data = {
+        CustomerId: $('#userId').val(),
+        FirstName: $('#firstName').val(),
+        LastName: $('#lastName').val(),
+        Email: $('#email').val(),
+        Password: $('#userpassword').val(),
+        PhoneNumber: $('#phone').val(),
+        Address: $('#Address').val(),
+        City: $('#City').val(),
+        State: $('#State').val(),
+        ZipCode: $('#ZipCode').val(),
+        Country: $('#Country').val(),
+        IsActive: $("input[type='radio'][name='radio']:checked").val()
+    };
+    var formData = new FormData();
+    formData.append('data', JSON.stringify(data));
+    $.ajax({
+        type: "POST",
+        url: '/User/SaveUser',
+        data: formData,
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data.ok) {
+                if (uid != null && uid != '') {
+                    alert('User edited sucessfully');
+                }
+                else {
+                    alert('User created sucessfully');
+                }
+                
+                setTimeout(function () {
+                    window.location.href = '/User/UserList';
+                }, 1000);
+            } else {
+                alert(data.errmsg);
+            }
+        }
+    });
+
+    return false;
+}
---
+++ AniBoard_Admin/wwwroot/js/pages/user.js
@@ -0,0 +1,58 @@
+﻿function searchuser() {
+    var searchText = $('#user_search').val();
+    if (searchText != null && searchText != '') {
+        //alert(searchText);
+        window.location.href = '/User/UserList?q=' + searchText;
+    }
+    else {
+        window.location.href = '/User/UserList';
+    }
+}
+
+function showaddedit_modal(userId) {
+    $("#user_body").empty();
+    
+    $.ajax({
+        url: "/User/ShowAddEditModal",
+        type: "GET",
+        data: { "userId": userId },
+        datatype: "json",
+        success: function (data) {
+            $("#user_body").html(data);
+        },
+    });
+}
+
+function deleteadminuser(userId) {
+    if (confirm("Are you sure you want to delete this user?")) {
+        var data = {
+            CustomerId: userId,
+            isDelete: 1
+        };
+        var formData = new FormData();
+        formData.append('data', JSON.stringify(data));
+        $.ajax({
+            type: "POST",
+            url: '/User/SaveUser',
+            data: formData,
+            dataType: 'json',
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                if (data.ok) {
+                    alert('User deleted sucessfully');
+                    setTimeout(function () {
+                        window.location.href = '/User/UserList';
+                    }, 1000);
+                } else {
+                    alert(data.errmsg);
+                }
+            }
+        });
+    }
+
+
+    return false;
+
+
+}
